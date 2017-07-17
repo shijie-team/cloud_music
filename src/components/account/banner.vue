5ab4dac59334d00869dfe70193bba367137915f2
wupeng <1063528563@qq.com>
2017-07-17 15:46:04 +0800

    'hh'

---
+++ src/components/account/banner.vue
@@ -0,0 +1,43 @@
+<template>
+	 <div>  
+        <swiper :options="swiperOption"  ref="mySwiper">  
+            <!-- 这部分放你要渲染的那些内容 -->  
+            <swiper-slide v-for="item in msg">  
+            	<img :src="item.picStr"/>
+            </swiper-slide>  
+            <!-- 这是轮播的小圆点 -->  
+            <div class="swiper-pagination" slot="pagination"></div>  
+        </swiper>  
+    </div> 
+</template>
+
+<script>
+import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+Vue.use(VueAwesomeSwiper)
+
+export default {
+    data () {
+      return {
+        swiperOption: {
+          autoplay: 1000,
+          loop: true,
+          autoplayDisableOnInteraction:false,
+          pagination: '.swiper-pagination'
+        }
+      }
+    },
+    components:{
+    	swiper,
+    	swiperSlide
+    },
+    props:{
+		msg:{}
+	}
+}
+</script>
+
+<style>
+	img{width: 100%;}
+</style>
--- src/views/Account/shop.vue
+++ src/views/Account/shop.vue
@@ -1,15 +1,17 @@
 <template>
 	<div id="shop">
 		<acch :msg='title'></acch>
+		<banner :msg='ban'></banner>
 	</div>
 </template>
 
 <script>
 import acch from '../../components/account/AccountHead'
+import banner from '../../components/account/banner'
 	export default {
 		name:'shop',
 		components:{
-			acch
+			acch,banner
 		},
 		data(){
 		  	return {
@@ -17,12 +19,15 @@ import acch from '../../components/account/AccountHead'
 					title:'云音乐商城',
 					tt:true,
 					myname:''
-				}
+				},
+				ban:""
 		  	}
 		},
-		mounted(){
+		created(){
 			this.$http.get('../../../static/json/banner.json').then(function(res){
 				console.log(res.body.banners);
+				this.ban=res.body.banners;
+				
 			});
 			this.$http.get('../../../static/json/allproducts.json').then(function(res){
 				console.log(res);
