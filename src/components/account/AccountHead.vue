dac7287f6691b5ffcfe8506539eeed61a9f43112
wupeng <1063528563@qq.com>
2017-07-17 21:38:08 +0800

    'h最后'

--- src/components/account/AccountHead.vue
+++ src/components/account/AccountHead.vue
@@ -2,7 +2,9 @@
 	<div id="accoHead">
 		<i class="iconfont icon-shang-copy-copy" v-show="msg.tt" @click='goback'></i>
 		<span>{{msg.title}}</span>
-		<i class="iconfont icon-dengji"></i>
+		<router-link to='/musicPlayer'>
+			<i class="iconfont icon-dengji"></i>
+		</router-link>
 	</div>
 </template>
 
@@ -28,7 +30,7 @@
 
 <style scoped>
 	#accoHead{height: 1.5rem;background: darkseagreen;line-height: 1.5rem;text-align: center;color: white;font-size: 0.5rem;}
-	.icon-dengji{float:right;width:1.5rem;font-size: .7rem;}
+	.icon-dengji{float:right;width:1.5rem;font-size: .7rem;color: white;}
 	#accoHead>span{margin-left: 1rem;}
 	.icon-shang-copy-copy{font-size: .7rem;float:left;width:1rem;}
 </style>
--- src/views/Account/accountchildren/Childmusic.vue
+++ src/views/Account/accountchildren/Childmusic.vue
@@ -12,25 +12,58 @@
 				</div>
 			</li>
 		</ul>
-		<p class="m-con"><b>收藏的歌单</b><span>(0)</span></p>
+		<p class="m-con"><b>收藏的歌单</b><span>({{songs.length}})</span>	</p>
+		<ul class="list">
+			<li v-for='item in songs' @click='play'>
+				<div class="pic pic1">
+					<img :src="item.cover"/>
+				</div>
+				<div class="songs-name">
+					<p>{{item.albumtitle}}</p>
+					<p>{{item.artist}}</p>
+				</div>
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script>
 	export default {
-		name:'childmusic'
+		name:'childmusic',
+		data(){
+			return {
+				songs:''
+			}
+		},
+		mounted(){
+			var songs = JSON.parse(localStorage.getItem('collectioned'));
+		    if(songs){
+		       this.songs = songs;
+		    }
+		},
+		methods:{
+			play(){
+				localStorage.setItem('singlegong',JSON.stringify(this.songs));
+				this.$router.push({path:'/musicPlayer'});
+			}
+		}
 	}
 </script>
 
 <style scoped>
 	.pic{width:2rem;height: 2rem;background:#333;color: white;text-align: center;}
 	.icon-dengji{font-size: 1rem;line-height: 2rem;}
-	.list>li{padding:.2rem .4rem .1rem .2rem;text-align:left;border-bottom: 1;}
-	.list>li>div{display: inline-block;}
+	.list li{padding:.2rem .4rem .1rem .2rem;text-align:left;border-bottom: 1;}
+	.list li div{display: inline-block;}
 	.m-con{height:.8rem;line-height: .8rem;background: lightgrey;}
 	.m-con>span,.m-con>b{float:left;}
 	.m-con>b{margin-left: .6rem;}
 	.list-name{padding-left: .2rem;border-bottom: 1px solid lightgray;height: 1.65rem;width: 70%;}
 	.list-name>p:nth-child(1){font-size: .45rem;}
 	.list-name>p:nth-child(2){font-size: .35rem;color:lightslategrey}
+	img{width:100%}
+	.pic1{background: transparent;}
+	.songs-name{vertical-align: top;}
+	.songs-name p{height: .6rem;line-height:.6rem;}
+	.songs-name p:nth-child(2){color:lightgray}
 </style>
